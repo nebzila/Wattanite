ab136bc4a315db8dbfe6c19995e6d5157826c4da
Ben <ben.stimpson@outlook.com>
2022-09-25 10:55:04 +0100

    start

--- src/Services/ApiClient.js
+++ src/Services/ApiClient.js
@@ -1,7 +1,7 @@
 //don't forget to hide your api key
-
-const movieURL =
-	'https://api.themoviedb.org/3/trending/movie/week?api_key=940dc4b555fdabad1a38b6f129d9b8f4';
+const movieKey = process.env.REACT_APP_MOVIE_API_KEY;
+console.log(process.env);
+const movieURL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${movieKey}`;
 export const getMovies = async () => {
 	const movies = await (await fetch(movieURL)).json();
 	const outPut = movies.results.map((movie) => {
---
+++ src/components/start-screen/start-screen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './start-screen.css';
+
+const StartScreen = () => {
+	return <h1> Start </h1>;
+};
+
+//export default
