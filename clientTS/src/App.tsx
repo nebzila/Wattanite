740be8ff5b28f4f2d6282b232096c28ad20635a9
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2022-09-29 19:58:04 +0100

    added: TS client

---
+++ clientTS/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Loading from './components/loading/loading';
+
+function App() {
+  return (
+    <div className="App">
+      <Loading></Loading>
+    </div>
+  );
+}
+
+export default App;
---
+++ clientTS/src/components/loading/loading.tsx
@@ -0,0 +1,5 @@
+function Loading() {
+  return ( <h1>Hello World</h1> );
+}
+
+export default Loading;
