03d7ee175b00af69d395c4892a671630dcc6948c
Simon Roope <simonroope@gmail.com>
2022-10-01 11:38:54 +0100

    props & typescript

--- clientTS/src/components/movie-option/movie-option.tsx
+++ clientTS/src/components/movie-option/movie-option.tsx
@@ -4,36 +4,38 @@ import { mainContext } from '../main-box/main-box';
 
 import { MovieType } from '../../allTypes';
 
-const MovieOption = ( props: MovieType ) => {
+const MovieOption = ( props ) => {
 	
 	const { setPage, setFormData, formData } = useContext(mainContext);
 
-	// function submitHandler(event, movieData) {
-	// 	event.preventDefault();
-	// 	setFormData({ ...formData, movie: movieData });
-	// 	setPage('restaurant');
-	// }
+	// props.value.title or formData.movie.title
+
+	function submitHandler(event, movieData) {
+		event.preventDefault();
+		setFormData({ ...formData, movie: movieData });
+		setPage('restaurant');
+	}
 
 	return (
 		<div className='movie-option'>
 			<div className='movie-left'>
 				<img
 					className='movie-poster'
-					src={'https://image.tmdb.org/t/p/w300/' + formData.movie.poster_path}
+					src={'https://image.tmdb.org/t/p/w300/' + props.value.poster_path}
 				/>
 			</div>
 			<div className='movie-right'>
 				<div className='movie-text'>
-					<h1 className='movie-title'>{formData.movie.title}</h1>
+					<h1 className='movie-title'>{props.value.title}</h1>
 					<h3>{formData.movie.vote_count}</h3>
-					<p className='movie-blurb'>{formData.movie.overview}</p>
+					<p className='movie-blurb'>{props.value.overview}</p>
 				</div>
 
 				<button
 					className='movie-vote-button'
-					// onClick={(event) => {
-					// 	submitHandler(event, formData.movie);
-					// }}
+					onClick={(event) => {
+					  submitHandler(event, formData.movie);
+					}}
 				>
 					+ 1
 				</button>
