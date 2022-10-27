03226825966e9a75e7c4091fece2d778721c41b5
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2022-10-27 09:56:09 +0100

    adding mocks

---
+++ client/src/test/mocks.js
@@ -0,0 +1,358 @@
+export const movies = {
+  results: [
+      {
+      adult: false,
+      backdrop_path: '/y2Ca1neKke2mGPMaHzlCNDVZqsK.jpg',
+      id: 718930,
+      title: 'Bullet Train',
+      original_language: 'en',
+      original_title: 'Bullet Train',
+      overview:
+        "Unlucky assassin Ladybug is determined to do his job peacefully after one too many gigs gone off the rails. Fate, however, may have other plans, as Ladybug's latest mission puts him on a collision course with lethal adversaries from around the globe—all with connected, yet conflicting, objectives—on the world's fastest train.",
+      poster_path: '/tVxDe01Zy3kZqaZRNiXFGDICdZk.jpg',
+      media_type: 'movie',
+      genre_ids: [28, 35, 53],
+      popularity: 7949.491,
+      release_date: '2022-07-03',
+      video: false,
+      vote_average: 7.495,
+      vote_count: 1252,
+    },
+    {
+      adult: false,
+      backdrop_path: '/o5Uy3zM1SNxzoMwy5H04GQAmfNF.jpg',
+      id: 301502,
+      title: 'Blonde',
+      original_language: 'en',
+      original_title: 'Blonde',
+      overview:
+        'From her volatile childhood as Norma Jeane, through her rise to stardom and romantic entanglements, this reimagined fictional portrait of Hollywood legend Marilyn Monroe blurs the lines of fact and fiction to explore the widening split between her public and private selves.',
+      poster_path: '/jOgbnL5FB30pprEjZaY1E1iPtPM.jpg',
+      media_type: 'movie',
+      genre_ids: [18],
+      popularity: 414.578,
+      release_date: '2022-09-16',
+      video: false,
+      vote_average: 6.664,
+      vote_count: 308,
+    },
+    {
+      adult: false,
+      backdrop_path: '/jsoz1HlxczSuTx0mDl2h0lxy36l.jpg',
+      id: 616037,
+      title: 'Thor: Love and Thunder',
+      original_language: 'en',
+      original_title: 'Thor: Love and Thunder',
+      overview:
+        'After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor Odinson enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher\'s vengeance and stop him before it\'s too late.',
+      poster_path: '/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg',
+      media_type: 'movie',
+      genre_ids: [14, 28, 35],
+      popularity: 2340.753,
+      release_date: '2022-07-06',
+      video: false,
+      vote_average: 6.76,
+      vote_count: 3963,
+    },
+    {
+      adult: false,
+      backdrop_path: '/z8KsgBFtduX5bS1yVkjoGo4L7KJ.jpg',
+      id: 597922,
+      title: 'The Greatest Beer Run Ever',
+      original_language: 'en',
+      original_title: 'The Greatest Beer Run Ever',
+      overview: 'Chickie wants to support his friends fighting in Vietnam, so he does something wild—personally bring them American beer. What starts as a well-meaning journey quickly changes Chickie\'s life and perspective. Based on a true story.',
+      poster_path: '/ggf37TpcKaxwguhvtNn6MQpyqBn.jpg',
+      media_type: 'movie',
+      genre_ids: [18, 35, 10752],
+      popularity: 163.832,
+      release_date: '2022-09-30',
+      video: false,
+      vote_average: 7.337,
+      vote_count: 46,
+    },
+    {
+      adult: false,
+      backdrop_path: '/sSrnyT3MqCBytcnBVigv5dwBx5w.jpg',
+      id: 642885,
+      title: 'Hocus Pocus 2',
+      original_language: 'en',
+      original_title: 'Hocus Pocus 2',
+      overview:
+        'It\'s been 29 years since someone lit the Black Flame Candle and resurrected the 17th-century sisters, and they are looking for revenge. Now it is up to three high-school students to stop the ravenous witches from wreaking a new kind of havoc on Salem before dawn on All Hallow\'s Eve.',
+      poster_path: '/7ze7YNmUaX81ufctGqt0AgHxRtL.jpg',
+      media_type: 'movie',
+      genre_ids: [14, 35, 10751],
+      popularity: 930.383,
+      release_date: '2022-09-27',
+      video: false,
+      vote_average: 7.963,
+      vote_count: 258,
+    },
+    {
+      adult: false,
+      backdrop_path: '/odJ4hx6g6vBt4lBWKFD1tI8WS4x.jpg',
+      id: 361743,
+      title: 'Top Gun: Maverick',
+      original_language: 'en',
+      original_title: 'Top Gun: Maverick',
+      overview:
+        'After more than thirty years of service as one of the Navy\'s top aviators, and dodging the advancement in rank that would ground him, Pete "Maverick" Mitchell finds himself training a detachment of TOP GUN graduates for a specialized mission the likes of which no living pilot has ever seen.',
+      poster_path: '/62HCnUTziyWcpDaBO2i1DX17ljH.jpg',
+      media_type: 'movie',
+      genre_ids: [28, 18],
+      popularity: 1580.086,
+      release_date: '2022-05-24',
+      video: false,
+      vote_average: 8.355,
+      vote_count: 4104,
+    },
+  ]
+};
+
+export const restaurants = {
+  results: [
+    {
+      business_status: 'OPERATIONAL',
+      formatted_address:
+        '28a Denbigh St, Lillington and Longmoore Gardens, London SW1V 2ER',
+      geometry: {
+        location: { lat: 51.4915756, lng: -0.1395575 },
+        viewport: {
+          northeast: { lat: 51.49291567989271, lng: -0.1382787701072778 },
+          southwest: { lat: 51.49021602010727, lng: -0.1409784298927222 },
+        },
+      },
+      icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png',
+      icon_background_color: '#FF9E67',
+      icon_mask_base_uri:
+        'https://maps.gstatic.com/mapfiles/place_api/icons/v2/restaurant_pinlet',
+      name: 'Noodle House',
+      opening_hours: { open_now: false },
+      photos: [
+        {
+          height: 360,
+          html_attributions: [
+            '<a href="https://maps.google.com/maps/contrib/101618324453744698815">A Google User</a>',
+          ],
+          photo_reference:
+            'AcYSjRh3VcA1sTcEa9SvPK-NGX25WP_Hglhq_VLBeARHVPp8DXC9p-WWCCKknHILnsUe5p_u6fqOJMy6eVtKM0jeobfXq6js3rJptqXgP6x7pjMrmqod-mx_3A09j_p74BI9i3b_xbKcTEun1IlAHxfGKZ2tqEmfjX-Imx0-TjchPUOjleEq',
+          width: 640,
+          imgsrc:
+            'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AcYSjRh3VcA1sTcEa9SvPK-NGX25WP_Hglhq_VLBeARHVPp8DXC9p-WWCCKknHILnsUe5p_u6fqOJMy6eVtKM0jeobfXq6js3rJptqXgP6x7pjMrmqod-mx_3A09j_p74BI9i3b_xbKcTEun1IlAHxfGKZ2tqEmfjX-Imx0-TjchPUOjleEq&key=AIzaSyC8eBAJ7v_XgjPaI1UI7Qc_d04CndplLFs',
+        },
+      ],
+      place_id: 'ChIJtyJqx3AFdkgRMFuz7SYq2zg',
+      plus_code: { compound_code: 'FVR6+J5 London', global_code: '9C3XFVR6+J5' },
+      rating: 2.5,
+      reference: 'ChIJtyJqx3AFdkgRMFuz7SYq2zg',
+      types: [
+        'meal_delivery',
+        'meal_takeaway',
+        'restaurant',
+        'food',
+        'point_of_interest',
+        'establishment',
+      ],
+      user_ratings_total: 12,
+    },
+    {
+      business_status: 'OPERATIONAL',
+      formatted_address: '46 Moreton St, Pimlico, London SW1V 2PB',
+      geometry: {
+        location: { lat: 51.4888913, lng: -0.1371379 },
+        viewport: {
+          northeast: { lat: 51.49019787989272, lng: -0.1357275201072778 },
+          southwest: { lat: 51.48749822010728, lng: -0.1384271798927222 },
+        },
+      },
+      icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png',
+      icon_background_color: '#FF9E67',
+      icon_mask_base_uri:
+        'https://maps.gstatic.com/mapfiles/place_api/icons/v2/restaurant_pinlet',
+      name: 'Pizza Express',
+      opening_hours: { open_now: false },
+      photos: [
+        {
+          height: 6000,
+          html_attributions: [
+            '<a href="https://maps.google.com/maps/contrib/108307118098204591929">Ar Tetrad</a>',
+          ],
+          photo_reference:
+            'AcYSjRh1ogC45C0LuVzLaqMiGP3_fyzq1YxAf4o6cajBjtndvpvUmBLGMT-xfWeVqhqrXcRmEHHTCVlAyEBHdkKxNENcwhjaEq9TDPCjEVpCj9yW41LB4uWERCw7fe-MVeumKrZraBVIgKWhx1ysbgL2s49GynyBahNnxt9uM0NCZry_fe_0',
+          width: 8000,
+          imgsrc:
+            'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AcYSjRh1ogC45C0LuVzLaqMiGP3_fyzq1YxAf4o6cajBjtndvpvUmBLGMT-xfWeVqhqrXcRmEHHTCVlAyEBHdkKxNENcwhjaEq9TDPCjEVpCj9yW41LB4uWERCw7fe-MVeumKrZraBVIgKWhx1ysbgL2s49GynyBahNnxt9uM0NCZry_fe_0&key=AIzaSyC8eBAJ7v_XgjPaI1UI7Qc_d04CndplLFs',
+        },
+      ],
+      place_id: 'ChIJcViTfOEEdkgRpavzoA7wSz4',
+      plus_code: { compound_code: 'FVQ7+H4 London', global_code: '9C3XFVQ7+H4' },
+      price_level: 2,
+      rating: 4,
+      reference: 'ChIJcViTfOEEdkgRpavzoA7wSz4',
+      types: [
+        'meal_takeaway',
+        'restaurant',
+        'food',
+        'point_of_interest',
+        'establishment',
+      ],
+      user_ratings_total: 423,
+    },
+    {
+      business_status: 'OPERATIONAL',
+      formatted_address:
+        '78 Wilton Rd, Lillington and Longmoore Gardens, London SW1V 1DL',
+      geometry: {
+        location: { lat: 51.49261509999999, lng: -0.1402076 },
+        viewport: {
+          northeast: { lat: 51.49394217989272, lng: -0.1389060701072778 },
+          southwest: { lat: 51.49124252010728, lng: -0.1416057298927222 },
+        },
+      },
+      icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png',
+      icon_background_color: '#FF9E67',
+      icon_mask_base_uri:
+        'https://maps.gstatic.com/mapfiles/place_api/icons/v2/restaurant_pinlet',
+      name: 'Chicken Joint & Taquitos Mexican Grill',
+      opening_hours: { open_now: false },
+      photos: [
+        {
+          height: 2268,
+          html_attributions: [
+            '<a href="https://maps.google.com/maps/contrib/111358601763148367939">Aziz Mehmet</a>',
+          ],
+          photo_reference:
+            'AcYSjRjDDKVYXrEaiQYGz4n-z3A3xMg1Dyjh1KngpEKAqPan40sJuxPsgAajaWLg7VWb44vrkKFRdr1VoC9H65t6nA9vosPjRGqxApgx4U7WcjM6LXX3dTgwIIeMlc7VkJnm_HURYd0kwYed2faoAntLfz-eA0g7egxfZj27M_R4aR7UpUhw',
+          width: 4032,
+          imgsrc:
+            'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AcYSjRjDDKVYXrEaiQYGz4n-z3A3xMg1Dyjh1KngpEKAqPan40sJuxPsgAajaWLg7VWb44vrkKFRdr1VoC9H65t6nA9vosPjRGqxApgx4U7WcjM6LXX3dTgwIIeMlc7VkJnm_HURYd0kwYed2faoAntLfz-eA0g7egxfZj27M_R4aR7UpUhw&key=AIzaSyC8eBAJ7v_XgjPaI1UI7Qc_d04CndplLFs',
+        },
+      ],
+      place_id: 'ChIJBdOJIuAEdkgRK-p_Og1S4AI',
+      plus_code: { compound_code: 'FVV5+2W London', global_code: '9C3XFVV5+2W' },
+      price_level: 1,
+      rating: 4.3,
+      reference: 'ChIJBdOJIuAEdkgRK-p_Og1S4AI',
+      types: [
+        'meal_takeaway',
+        'restaurant',
+        'food',
+        'point_of_interest',
+        'establishment',
+      ],
+      user_ratings_total: 245,
+    },
+    {
+      business_status: 'OPERATIONAL',
+      formatted_address:
+        '28 Denbigh St, Lillington and Longmoore Gardens, London SW1V 2ER',
+      geometry: {
+        location: { lat: 51.4915709, lng: -0.139551 },
+        viewport: {
+          northeast: { lat: 51.49292322989272, lng: -0.1382745701072778 },
+          southwest: { lat: 51.49022357010728, lng: -0.1409742298927222 },
+        },
+      },
+      icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png',
+      icon_background_color: '#FF9E67',
+      icon_mask_base_uri:
+        'https://maps.gstatic.com/mapfiles/place_api/icons/v2/restaurant_pinlet',
+      name: 'Yum Yum (Pimlico)',
+      opening_hours: { open_now: false },
+      photos: [
+        {
+          height: 2136,
+          html_attributions: [
+            '<a href="https://maps.google.com/maps/contrib/100999618683740154683">celine wathier</a>',
+          ],
+          photo_reference:
+            'AcYSjRiBcVkdOoLykxjHFZZc3CK8tJd8g3NtV4ufM1SeiMaTpuDVixOmkw6vqkSdG-5IuSpTP-6d-RyKQTomfHkQMuuLqYfNDQE4JcloexDZN_xmyYM2W3AG3pUogI5lmbK7qmZuA31Z6JyARWMlm-H7FkSP9yW5kodrwIGXeB1VsvxGjQbK',
+          width: 4624,
+          imgsrc:
+            'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AcYSjRiBcVkdOoLykxjHFZZc3CK8tJd8g3NtV4ufM1SeiMaTpuDVixOmkw6vqkSdG-5IuSpTP-6d-RyKQTomfHkQMuuLqYfNDQE4JcloexDZN_xmyYM2W3AG3pUogI5lmbK7qmZuA31Z6JyARWMlm-H7FkSP9yW5kodrwIGXeB1VsvxGjQbK&key=AIzaSyC8eBAJ7v_XgjPaI1UI7Qc_d04CndplLFs',
+        },
+      ],
+      place_id: 'ChIJz815NuAEdkgRA9yk6-Aa7og',
+      plus_code: { compound_code: 'FVR6+J5 London', global_code: '9C3XFVR6+J5' },
+      price_level: 2,
+      rating: 2.5,
+      reference: 'ChIJz815NuAEdkgRA9yk6-Aa7og',
+      types: [
+        'meal_delivery',
+        'meal_takeaway',
+        'restaurant',
+        'food',
+        'point_of_interest',
+        'establishment',
+      ],
+      user_ratings_total: 228,
+    },
+  ]
+};
+​
+export const vote = {
+  name: 'Alex', postcode: 'SW3',
+  movie: {
+      adult: false,
+      backdrop_path: '/y2Ca1neKke2mGPMaHzlCNDVZqsK.jpg',
+      id: 718930,
+      title: 'Bullet Train',
+      original_language: 'en',
+      original_title: 'Bullet Train',
+      overview:
+        "Unlucky assassin Ladybug is determined to do his job peacefully after one too many gigs gone off the rails. Fate, however, may have other plans, as Ladybug's latest mission puts him on a collision course with lethal adversaries from around the globe—all with connected, yet conflicting, objectives—on the world's fastest train.",
+      poster_path: '/tVxDe01Zy3kZqaZRNiXFGDICdZk.jpg',
+      media_type: 'movie',
+      genre_ids: [28, 35, 53],
+      popularity: 7949.491,
+      release_date: '2022-07-03',
+      video: false,
+      vote_average: 7.495,
+      vote_count: 1252,
+    },
+  restaurant: {  business_status: 'OPERATIONAL',
+  formatted_address:
+    '28a Denbigh St, Lillington and Longmoore Gardens, London SW1V 2ER',
+  geometry: {
+    location: { lat: 51.4915756, lng: -0.1395575 },
+    viewport: {
+      northeast: { lat: 51.49291567989271, lng: -0.1382787701072778 },
+      southwest: { lat: 51.49021602010727, lng: -0.1409784298927222 },
+    },
+  },
+  icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png',
+  icon_background_color: '#FF9E67',
+  icon_mask_base_uri:
+    'https://maps.gstatic.com/mapfiles/place_api/icons/v2/restaurant_pinlet',
+  name: 'Noodle House',
+  opening_hours: { open_now: false },
+  photos: [
+    {
+      height: 360,
+      html_attributions: [
+        '<a href="https://maps.google.com/maps/contrib/101618324453744698815">A Google User</a>',
+      ],
+      photo_reference:
+        'AcYSjRh3VcA1sTcEa9SvPK-NGX25WP_Hglhq_VLBeARHVPp8DXC9p-WWCCKknHILnsUe5p_u6fqOJMy6eVtKM0jeobfXq6js3rJptqXgP6x7pjMrmqod-mx_3A09j_p74BI9i3b_xbKcTEun1IlAHxfGKZ2tqEmfjX-Imx0-TjchPUOjleEq',
+      width: 640,
+      imgsrc:
+        'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AcYSjRh3VcA1sTcEa9SvPK-NGX25WP_Hglhq_VLBeARHVPp8DXC9p-WWCCKknHILnsUe5p_u6fqOJMy6eVtKM0jeobfXq6js3rJptqXgP6x7pjMrmqod-mx_3A09j_p74BI9i3b_xbKcTEun1IlAHxfGKZ2tqEmfjX-Imx0-TjchPUOjleEq&key=AIzaSyC8eBAJ7v_XgjPaI1UI7Qc_d04CndplLFs',
+    },
+  ],
+  place_id: 'ChIJtyJqx3AFdkgRMFuz7SYq2zg',
+  plus_code: { compound_code: 'FVR6+J5 London', global_code: '9C3XFVR6+J5' },
+  price_level: 2,
+  rating: 2.5,
+  reference: 'ChIJtyJqx3AFdkgRMFuz7SYq2zg',
+  types: [
+    'meal_delivery',
+    'meal_takeaway',
+    'restaurant',
+    'food',
+    'point_of_interest',
+    'establishment',
+  ],
+  user_ratings_total: 12,
+}
+}
